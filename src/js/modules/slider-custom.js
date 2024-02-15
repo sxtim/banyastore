56a86a1ba599cb263d59794c5266282d6580b642
sxtim <sxtim@gmail.com.com>
2024-02-16 00:32:27 +0300

    added responsiveness to the slider

--- src/js/modules/slider-custom.js
+++ src/js/modules/slider-custom.js
@@ -1,36 +1,72 @@
-const slider = document.querySelector('.top-slider');
-const sliderImages = document.querySelectorAll('.top-slider__img');
-const sliderLine = document.querySelector('.slider__line');
+// Исходные данные по слайдеру (const)
+const sliderImages = document.querySelectorAll('.slider__img'),
+    sliderLine = document.querySelector('.slider__line'),
+    sliderDots = document.querySelectorAll('.slider__dot'),
+    sliderBtnNext = document.querySelector('.slider__btn-next'),
+    sliderBtnPrev = document.querySelector('.slider__btn-prev');
 
-const sliderBtnNext = document.querySelector('.slider__btn-next');
-const sliderBtnPrev = document.querySelector('.slider__btn-prev');
+// Переменные
+let sliderCount = 0,
+    sliderWidth;
 
-let sliderCount = 0;
-let sliderWidth = slider.offsetWidth;
+// Адаптивность слайдера
+window.addEventListener('resize', showSlide);
 
+// Кнопки листания слайдов вперед и назад
 sliderBtnNext.addEventListener('click', nextSlide);
 sliderBtnPrev.addEventListener('click', prevSlide);
 
+// Автоматическое перелистывание слайдов
+// setInterval(() => {
+//     nextSlide()
+// }, 3000);
+
+
+// Функции ==================
+// Задает нужную ширину картинки и sliderLine
+function showSlide() {
+    sliderWidth = document.querySelector('.slider').offsetWidth;
+    sliderLine.style.width = sliderWidth * sliderImages.length + 'px';
+    sliderImages.forEach(item => item.style.width = sliderWidth + 'px');
+
+    rollSlider();
+}
+showSlide();
+
+// Перелистывает слайд вперед
 function nextSlide() {
     sliderCount++;
-    if (sliderCount >= sliderImages.length) {
-        sliderCount = 0;
-    }
-    rollSlide();
+    if (sliderCount >= sliderImages.length) sliderCount = 0;
+
+    rollSlider();
+    thisSlide(sliderCount);
 }
 
+// Перелистывает слайд назад
 function prevSlide() {
     sliderCount--;
-    if (sliderCount <= 0) {
-        sliderCouint = sliderImages.length - 1;
-    }
-    rollSlide();
+    if (sliderCount < 0) sliderCount = sliderImages.length -1;
+
+    rollSlider();
+    thisSlide(sliderCount);
 }
 
-function rollSlide() {
+// Задает шаг перемещения слайдов
+function rollSlider() {
     sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
 }
 
-setInterval(() => {
-    nextSlide()
-}, 3000);
+// Указывает как слайд по счету активен
+function thisSlide(index) {
+    sliderDots.forEach(item => item.classList.remove('active-dot'));
+    sliderDots[index].classList.add('active-dot');
+}
+
+// Вешает клик на dot
+sliderDots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        sliderCount = index;
+        rollSlider();
+        thisSlide(sliderCount);
+    })
+})
