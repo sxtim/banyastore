30b3eea1aed65383d43d1cd93457f9923439e563
sxtim <sxtim@gmail.com.com>
2024-02-18 02:46:02 +0300

    add keen slider to top

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // import mobileNav from './modules/mobile-nav.js';
 // mobileNav();
 
-import  './modules/slider-custom';
+// import  './modules/slider-custom';
+import  './modules/slider-k-top';
 
 
---
+++ src/js/modules/slider-k-top.js
@@ -0,0 +1,128 @@
+import KeenSlider from 'keen-slider'
+import 'keen-slider/keen-slider.min.css'
+
+function navigation(slider) {
+    let wrapper, dots, arrowLeft, arrowRight
+    let timeout
+    let mouseOver = false
+    function clearNextTimeout() {
+        clearTimeout(timeout)
+    }
+    function nextTimeout() {
+        clearTimeout(timeout)
+        if (mouseOver) return
+        timeout = setTimeout(() => {
+            slider.next()
+        }, 2000)
+    }
+    slider.on("created", () => {
+        slider.container.addEventListener("mouseover", () => {
+            mouseOver = true
+            clearNextTimeout()
+        })
+        slider.container.addEventListener("mouseout", () => {
+            mouseOver = false
+            nextTimeout()
+        })
+        nextTimeout()
+    })
+    slider.on("dragStarted", clearNextTimeout)
+    slider.on("animationEnded", nextTimeout)
+    slider.on("updated", nextTimeout)
+
+    function markup(remove) {
+        wrapperMarkup(remove)
+        dotMarkup(remove)
+        arrowMarkup(remove)
+    }
+
+    function removeElement(elment) {
+        elment.parentNode.removeChild(elment)
+    }
+    function createDiv(className) {
+        var div = document.createElement("div")
+        var classNames = className.split(" ")
+        classNames.forEach((name) => div.classList.add(name))
+        return div
+    }
+
+    function arrowMarkup(remove) {
+        if (remove) {
+            removeElement(arrowLeft)
+            removeElement(arrowRight)
+            return
+        }
+        arrowLeft = createDiv("arrow arrow--left")
+        arrowLeft.addEventListener("click", () => slider.prev())
+        arrowRight = createDiv("arrow arrow--right")
+        arrowRight.addEventListener("click", () => slider.next())
+
+        wrapper.appendChild(arrowLeft)
+        wrapper.appendChild(arrowRight)
+    }
+
+    function wrapperMarkup(remove) {
+        if (remove) {
+            var parent = wrapper.parentNode
+            while (wrapper.firstChild)
+                parent.insertBefore(wrapper.firstChild, wrapper)
+            removeElement(wrapper)
+            return
+        }
+        wrapper = createDiv("keen-slider__wrapper")
+        slider.container.parentNode.appendChild(wrapper)
+        wrapper.appendChild(slider.container)
+    }
+
+    function dotMarkup(remove) {
+        if (remove) {
+            removeElement(dots)
+            return
+        }
+        dots = createDiv("dots")
+        slider.track.details.slides.forEach((_e, idx) => {
+            var dot = createDiv("dot")
+            dot.addEventListener("click", () => slider.moveToIdx(idx))
+            dots.appendChild(dot)
+        })
+        wrapper.appendChild(dots)
+    }
+
+    function updateClasses() {
+        var slide = slider.track.details.rel
+        slide === 0
+            ? arrowLeft.classList.add("arrow--disabled")
+            : arrowLeft.classList.remove("arrow--disabled")
+        slide === slider.track.details.slides.length - 1
+            ? arrowRight.classList.add("arrow--disabled")
+            : arrowRight.classList.remove("arrow--disabled")
+        Array.from(dots.children).forEach(function (dot, idx) {
+            idx === slide
+                ? dot.classList.add("dot--active")
+                : dot.classList.remove("dot--active")
+        })
+    }
+
+    slider.on("created", () => {
+        markup()
+        updateClasses()
+    })
+    slider.on("optionsChanged", () => {
+        console.log(2)
+        markup(true)
+        markup()
+        updateClasses()
+    })
+    slider.on("slideChanged", () => {
+        updateClasses()
+    })
+    slider.on("destroyed", () => {
+        markup(true)
+    })
+}
+
+var slider = new KeenSlider("#keen-slider-top", {
+    loop: true,
+
+}, [navigation])
+
